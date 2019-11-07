0899d80837136824e71fe0803126b2cae2374c21
Aporoid <Aporoid@hotmail.com>
2019-11-06 19:19:37 -0600

    Tried making SFX working
    For some reason, it refused to acknowledge the sounds themselves, even after amplification.

--- PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/CollectItem.cs
+++ PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/CollectItem.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class CollectItem : MonoBehaviour
 {
-    
-
-    void OnTriggerEnter(Collider other)
+	void OnTriggerEnter(Collider other)
     {
         // Increasing the number of the item by 1 each time one is collected then getting rid of the object
         Inventory.KeycardAmount += 1;
-        Destroy(gameObject);
+		Destroy(gameObject);
     }
 }
