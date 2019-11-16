8b749dfdd1d40f08cd97513e096e00cc8f964641
Aporoid <Aporoid@hotmail.com>
2019-11-16 11:55:57 -0600

    Starting work on Terminal access

---
+++ PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/TerminalInteract.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TerminalInteract : MonoBehaviour
+{
+	[SerializeField]
+	private Canvas hudController;
+	[SerializeField]
+	private Canvas terminalCanvas;
+	[SerializeField]
+	private Text interactText;
+
+	[SerializeField]
+	private FirstPersonController fpsController;
+
+	private void Awake()
+	{
+		terminalCanvas.enabled = false;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		interactText.text = "Press [F] to access Terminal";
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (Input.GetButtonDown("Interact"))
+		{
+			
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		interactText.text = "";
+	}
+
+	private void AccessTerminal()
+	{
+		hudController.enabled = false;
+		terminalCanvas.enabled = true;
+		fpsController.enabled = false;
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	public void CloseTerminal()
+	{
+		fpsController.enabled = true;
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		hudController.enabled = true;
+		terminalCanvas.enabled = false;
+	}
+}
