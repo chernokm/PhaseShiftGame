23bf084d70eaa7c9ccbe11c0fd80712c1a9a2fba
Chris Grych <ceggrych@gmail.com>
2020-02-05 12:45:14 -0600

    Refactors raycast class

--- PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/PlayerRaycastInteract.cs
+++ PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/PlayerRaycastInteract.cs
@@ -8,7 +8,7 @@ public class PlayerRaycastInteract : MonoBehaviour
     [SerializeField] private float maxInteractDistance = 2f;
     [SerializeField] private UnityEngine.UI.Text interactText;
 
-    private enum LookStates { Nothing, FoundObject, IsLookingAtObject, LostObject }
+    private enum LookStates { Nothing, IsLookingAtObject, LostObject }
     private LookStates currentState;
 
     private DoorEvents interactObject;
@@ -24,67 +24,61 @@ public class PlayerRaycastInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleStates();
         CastRay();
     }
 
-    private void HandleStates()
+    private void CastRay() // Then apply to all other interactables, then make sensible comments!!
     {
-        switch (currentState)
-        {
-            case LookStates.FoundObject:
-                currentState = LookStates.IsLookingAtObject;
-                break;
-            case LookStates.LostObject:
-                currentState = LookStates.Nothing;
-                break;
-        }
-    }
-
-    private void CastRay() // !! Check Efficiency!! Then apply to all other interactables, then make sensible comments!!
-    {
-        RaycastHit hit; // Used to store what the ray hits
         Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward); // Casts a ray from the forward center of camera
 
-        switch (currentState)
+        switch (currentState) // Further behavior depends on what the lookstate is currently doing
         {
             case LookStates.Nothing:
-                if (Physics.Raycast(ray, out hit, maxInteractDistance)) // When the casted out ray with limited distance var (maxInteractDis) hits something
-                {
-                    switch (hit.collider.transform.tag) // What the ray is hitting. ie, what the player is looking at within Interact distance
-                    {
-                        case "Interact":
-                            currentState = LookStates.FoundObject;
-                            SwitchToFoundObject(hit);
-                            currentState = LookStates.IsLookingAtObject;
-                            break;
-                    }
-                }
+                ObjectCatchesRay(ray);
                 break;
+
             case LookStates.IsLookingAtObject:
-                if (Input.GetButtonDown("Interact"))
-                {
-                    interactObject.Pickup();
-                }
-                if (!Physics.Raycast(ray, out hit, maxInteractDistance))
-                {
-                    currentState = LookStates.LostObject;
-                    interactText.text = "";
-                }
+                EnableAndDisableUIPrompt(ray);
+                break;
+
+            case LookStates.LostObject:
+                currentState = LookStates.Nothing; // When the player loses focus on the object, the state turns to "lose object" and empties the interact text. Immediately changing the state the very next frame makes it so the interact text only empties once, the frame when the player stops focusing on said object
                 break;
         }
     }
 
-    private void SwitchToFoundObject(RaycastHit hit)
+    private void ObjectCatchesRay(Ray ray)
     {
-        interactObject = hit.collider.gameObject.GetComponent<DoorEvents>();
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxInteractDistance)) // When the casted out ray with limited distance var (maxInteractDis) hits something
+        {
+            switch (hit.collider.transform.tag) // What the ray is hitting. ie, what the player is looking at within Interact distance. This is tag based.
+            {
+                case "Interact": // If the object's tag is "Interact"
+                    AssignInteractiveObject(hit);
+                    currentState = LookStates.IsLookingAtObject;
+                    break;
+            }
+        }
+    }
+
+    private void EnableAndDisableUIPrompt(Ray ray)
+    {
+        RaycastHit hit;
         if (Input.GetButtonDown("Interact"))
         {
             interactObject.Pickup();
         }
-        else
+        if (!Physics.Raycast(ray, out hit, maxInteractDistance)) // If the player stop looking at interactive object, interact text empties out once then this method stops executing until another object is found
         {
-            interactObject.ShowUIText();
+            currentState = LookStates.LostObject;
+            interactText.text = "";
         }
+    }
+
+    private void AssignInteractiveObject(RaycastHit hit)
+    {
+        interactObject = hit.collider.gameObject.GetComponent<DoorEvents>(); // When the ray catches an interactive object, the unassigned DoorEvents var of this class gets assigned to that object. This only happens one frame when the player focuses on an object.
+        interactObject.ShowUIText();
     }
 }
