949c9ad0055f5fb2af15cf68ea37ad6872ff613e
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2020-02-14 15:37:16 -0600

    Audio Test Script
    Created AudioTest Script to test if the Banks have loaded properly and if sounds will play

---
+++ PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/Audio/AudioTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sx_test", GetComponent<Transform>().position);
+        //debug
+        Debug.Log("You should hear squish sound");
+    }
+}
