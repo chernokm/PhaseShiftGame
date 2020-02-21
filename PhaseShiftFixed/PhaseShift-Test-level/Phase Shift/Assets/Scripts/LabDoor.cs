07fb259833db3b5d2d41a81a5da6b75b5a64cbf8
eolsonp <eolsonp@gmail.com>
2020-02-21 09:53:01 -0600

    Doors Work!

---
+++ PhaseShiftFixed/PhaseShift-Test-level/Phase Shift/Assets/Scripts/LabDoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabDoor : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+       if  (col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            Doors("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (doorOpen)
+            {
+                doorOpen = false;
+                Doors("Close");
+            }
+        }
+    }
+
+    void Doors(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
